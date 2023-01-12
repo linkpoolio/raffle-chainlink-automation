c1dcacf142731e1f09100f6f1e2c5b3b3ccf80ba
jongregis <jon.gregis@gmail.com>
2023-01-12 10:59:24 -0600

    add fee token logic

--- test/Raffle.ts
+++ test/Raffle.ts
@@ -9,7 +9,7 @@ enum RaffleState {
   live,
   finished,
 }
-
+const zeroAddress = ethers.constants.AddressZero;
 describe("Raffle", function () {
   let keyHash: string,
     callbackGasLimit: number,
@@ -52,13 +52,14 @@ describe("Raffle", function () {
       owner.address,
     ]);
     await vrfCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address);
-    const prizeStruct = { prizeName: "N64" };
+    const prizeStruct = { prizeName: "N64", claimed: false };
     const name = ethers.utils.formatBytes32String("first one");
     await raffle.createRaffle(
       prizeStruct,
       60,
       ethers.utils.parseEther("1"),
       name,
+      zeroAddress,
       { value: ethers.utils.parseEther("1") }
     );
   });
@@ -207,6 +208,104 @@ describe("Raffle", function () {
       tx = await vrfCoordinatorV2Mock.fulfillRandomWords(1, raffle.address);
       await tx.wait(1);
       await expect(raffle.claimPrize(0)).to.emit(raffle, "RafflePrizeClaimed");
+    });
+  });
+});
+
+describe("Raffle with fee token", function () {
+  let keyHash: string,
+    callbackGasLimit: number,
+    requestConfirmations: number,
+    subscriptionId: any,
+    raffle: any,
+    vrfCoordinatorV2Mock: any,
+    erc20WETHMock: any,
+    _owner: any,
+    acct2: any,
+    acct3: any;
+
+  beforeEach(async () => {
+    const [owner, account2, account3] = await ethers.getSigners();
+    const BASE_FEE = "2500000000";
+    const GAS_PRICE_LINK = 1e9;
+    acct2 = account2;
+    acct3 = account3;
+    _owner = owner;
+    keyHash =
+      "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15";
+    callbackGasLimit = 10000000;
+    requestConfirmations = 3;
+    vrfCoordinatorV2Mock = await deploy("VRFCoordinatorV2Mock", [
+      BASE_FEE,
+      GAS_PRICE_LINK,
+    ]);
+    erc20WETHMock = await deploy("ERC20Mock", ["Wrapped ETH", "WETH"]);
+    const tx = await vrfCoordinatorV2Mock.createSubscription();
+    let txReceipt = await tx.wait(1);
+    subscriptionId = txReceipt.events[0].args.subId;
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId.toNumber(),
+      ethers.utils.parseEther("5")
+    );
+    raffle = await deploy("Raffle", [
+      vrfCoordinatorV2Mock.address,
+      subscriptionId,
+      requestConfirmations,
+      callbackGasLimit,
+      keyHash,
+      owner.address,
+    ]);
+    await vrfCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address);
+    const prizeStruct = { prizeName: "N64", claimed: false };
+    const name = ethers.utils.formatBytes32String("first one");
+    await raffle.createRaffle(
+      prizeStruct,
+      60,
+      ethers.utils.parseEther("1"),
+      name,
+      erc20WETHMock.address
+    );
+  });
+
+  describe("using tokens for fees", function () {
+    it("create raffle with fee token", async () => {
+      const currentRaffle = await raffle.raffles(0);
+      assert(currentRaffle.feeTokenAddress == erc20WETHMock.address);
+    });
+    it("enter raffle that requires a fee token", async () => {
+      await erc20WETHMock.approve(raffle.address, ethers.utils.parseEther("1"));
+      await raffle.enterRaffle(0, 1);
+    });
+    it("should fail if not approved", async () => {
+      await erc20WETHMock.approve(raffle.address, ethers.utils.parseEther("1"));
+      await expect(raffle.enterRaffle(0, 2)).to.be.revertedWith(
+        "ERC20: insufficient allowance"
+      );
+    });
+    it("able to claim fee tokens after raffle is done", async () => {
+      await erc20WETHMock.approve(raffle.address, ethers.utils.parseEther("1"));
+      await raffle.enterRaffle(0, 1);
+      await network.provider.send("evm_increaseTime", [100000000]);
+      await network.provider.request({ method: "evm_mine", params: [] });
+      let tx = await raffle.performUpkeep("0x");
+      await tx.wait(1);
+      tx = await vrfCoordinatorV2Mock.fulfillRandomWords(1, raffle.address);
+      await tx.wait(1);
+      await expect(raffle.claimPrize(0)).to.emit(raffle, "RafflePrizeClaimed");
+    });
+    it("should fail to claim winnings after already claimed", async () => {
+      await erc20WETHMock.approve(raffle.address, ethers.utils.parseEther("1"));
+      await raffle.enterRaffle(0, 1);
+      await network.provider.send("evm_increaseTime", [100000000]);
+      await network.provider.request({ method: "evm_mine", params: [] });
+      let tx = await raffle.performUpkeep("0x");
+      await tx.wait(1);
+      tx = await vrfCoordinatorV2Mock.fulfillRandomWords(1, raffle.address);
+      await tx.wait(1);
+      await expect(raffle.claimPrize(0)).to.emit(raffle, "RafflePrizeClaimed");
+      await expect(raffle.claimPrize(0)).to.be.revertedWith(
+        "Prize has already been claimed"
+      );
     });
   });
 });
