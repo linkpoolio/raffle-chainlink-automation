ca225f505c499f5d44525dcf91a8c9588210322a
jongregis <jon.gregis@gmail.com>
2023-02-10 11:53:48 -0600

    remove hardhat

--- test/utils/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { ethers } from "hardhat";
-
-export const deploy = async (contractName: string, args: any[] = []) => {
-  const Contract = await ethers.getContractFactory(contractName);
-  return Contract.deploy(...args);
-};
