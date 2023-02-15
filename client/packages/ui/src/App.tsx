e374b772cdd2fe9b2e93a58aa98279f0973a176c
ryanostrom <rpostrom@gmail.com>
2023-02-15 12:22:10 -0500

    Install typescript, jest tests, snapshots, docker build with nginx, and github publish action

---
+++ client/packages/ui/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { Routes } from '@ui/Routes'
+import { Nav } from '@ui/features/nav'
+import { RaffleList, RaffleDetail } from '@ui/features/raffle'
+
+export const App = () => (
+  <div data-testid="app">
+    <Nav />
+    <Switch>
+      <Route exact path={Routes.RaffleList}>
+        <RaffleList />
+      </Route>
+      <Route exact path={Routes.RaffleDetail}>
+        <RaffleDetail />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  </div>
+)
