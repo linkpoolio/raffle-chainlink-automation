758b40be97b6ddde62d188976ae964b7b88c171a
MG <1marc.ghannam@gmail.com>
2023-02-22 09:30:11 -0500

    feat: Use wagmi/core for write methods

--- client/packages/ui/src/api/contracts/writeFunctions.ts
+++ client/packages/ui/src/api/contracts/writeFunctions.ts
@@ -1,4 +1,4 @@
-import { useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { prepareWriteContract, writeContract } from '@wagmi/core'
 
 import { ethers } from 'ethers'
 
@@ -10,9 +10,9 @@ import linkTokenABI from './abi/LinkToken.json'
 const raffleManagerContractAddress = env.raffleManagerContractAddress()
 const linkTokenContractAddress = env.linkTokenContractAddress()
 
-export const createRaffle = (params: contracts.CreateRaffleParams) => {
+export const createRaffle = async (params: contracts.CreateRaffleParams) => {
   try {
-    const { config } = usePrepareContractWrite({
+    const config = await prepareWriteContract({
       address: raffleManagerContractAddress,
       abi: raffleManagerABI,
       functionName: 'createRaffle',
@@ -29,49 +29,46 @@ export const createRaffle = (params: contracts.CreateRaffleParams) => {
         params.entriesPerUser ? params.entriesPerUser : 1
       ]
     })
-    const { data, isLoading, isSuccess } = useContractWrite(config)
-    return { data, isLoading, isSuccess }
+    return writeContract(config)
   } catch (error: any) {
     throw new Error(`Error creating raffle: ${error.message}`)
   }
 }
 
-export const enterRaffle = (params: contracts.EnterRaffleParams) => {
+export const enterRaffle = async (params: contracts.EnterRaffleParams) => {
   try {
     const { id, proof } = params
-    const { config } = usePrepareContractWrite({
+    const config = await prepareWriteContract({
       address: raffleManagerContractAddress,
       abi: raffleManagerABI,
       functionName: 'enterRaffle',
       args: [id, params.entries ? params.entries : 1, proof ? proof : []]
     })
-    const { data, isLoading, isSuccess } = useContractWrite(config)
-    return { data, isLoading, isSuccess }
+    return writeContract(config)
   } catch (error: any) {
     throw new Error(`Error entering raffle: ${error.message}`)
   }
 }
 
-export const claimPrize = (params: contracts.ClaimPrizeParams) => {
+export const claimPrize = async (params: contracts.ClaimPrizeParams) => {
   try {
     const { id } = params
-    const { config } = usePrepareContractWrite({
+    const config = await prepareWriteContract({
       address: raffleManagerContractAddress,
       abi: raffleManagerABI,
       functionName: 'claimPrize',
       args: [id]
     })
-    const { data, isLoading, isSuccess } = useContractWrite(config)
-    return { data, isLoading, isSuccess }
+    return writeContract(config)
   } catch (error: any) {
     throw new Error(`Error claiming prize: ${error.message}`)
   }
 }
 
-export const resolveRaffle = (params: contracts.ResolveRaffleParams) => {
+export const resolveRaffle = async (params: contracts.ResolveRaffleParams) => {
   try {
     const { value, id } = params
-    const { config } = usePrepareContractWrite({
+    const config = await prepareWriteContract({
       address: linkTokenContractAddress,
       abi: linkTokenABI,
       functionName: 'transferAndCall',
@@ -81,24 +78,22 @@ export const resolveRaffle = (params: contracts.ResolveRaffleParams) => {
         ethers.utils.solidityPack(['uint256'], [id])
       ]
     })
-    const { data, isLoading, isSuccess } = useContractWrite(config)
-    return { data, isLoading, isSuccess }
+    return writeContract(config)
   } catch (error: any) {
     throw new Error(`Error resolving raffle: ${error.message}`)
   }
 }
 
-export const withdrawLink = (params: contracts.WithdrawLinkParams) => {
+export const withdrawLink = async (params: contracts.WithdrawLinkParams) => {
   try {
     const { id } = params
-    const { config } = usePrepareContractWrite({
+    const config = await prepareWriteContract({
       address: raffleManagerContractAddress,
       abi: raffleManagerABI,
       functionName: 'withdrawLink',
       args: [id]
     })
-    const { data, isLoading, isSuccess } = useContractWrite(config)
-    return { data, isLoading, isSuccess }
+    return writeContract(config)
   } catch (error: any) {
     throw new Error(`Error withdrawing link: ${error.message}`)
   }
