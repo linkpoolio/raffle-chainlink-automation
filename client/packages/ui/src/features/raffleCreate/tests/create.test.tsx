bc91545f4c5de17eb1859a33a6bf9a641b55f9cb
jongregis <jon.gregis@gmail.com>
2023-03-05 10:37:02 -0600

    fix lint

--- client/packages/ui/src/features/raffleCreate/tests/create.test.tsx
+++ client/packages/ui/src/features/raffleCreate/tests/create.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+// import renderer from 'react-test-renderer'
 import { render, screen } from '@testing-library/react'
 // import userEvent from '@testing-library/user-event'
 import { Providers } from '@ui/providers'
