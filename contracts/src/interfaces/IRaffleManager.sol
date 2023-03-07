7a55a7b2f8fed11ab1300f7bca0594edab6fc024
MG <1marc.ghannam@gmail.com>
2023-03-07 11:22:52 -0500

    fix: Add pause unpause to interface

--- contracts/src/interfaces/IRaffleManager.sol
+++ contracts/src/interfaces/IRaffleManager.sol
@@ -2,21 +2,36 @@
 pragma solidity ^0.8.17;
 
 interface IRaffleManager {
-    function enterRaffle(uint256 raffleId, uint8 entries, bytes32[] memory proof) external payable;
+    function enterRaffle(
+        uint256 raffleId,
+        uint8 entries,
+        bytes32[] memory proof
+    ) external payable;
 
-    function getWinners(uint256 raffleId) external view returns (bytes32[] memory);
+    function getWinners(uint256 raffleId)
+        external
+        view
+        returns (bytes32[] memory);
 
     function claimPrize(uint256 raffleId) external;
 
     function setKeeperRegistryAddress(address newKeeperAddress) external;
 
-    function setProvenanceHash(uint256 raffleId, bytes memory provenanceHash) external;
+    function setProvenanceHash(uint256 raffleId, bytes memory provenanceHash)
+        external;
 
     function withdrawLink(uint256 raffleId) external;
 
-    function claimableLink(uint256 raffleId) external view returns (uint256 claimable);
+    function claimableLink(uint256 raffleId)
+        external
+        view
+        returns (uint256 claimable);
 
     function updateRaffleOwner(uint256 raffleId, address newAdmin) external;
 
     function isPaused() external view returns (bool);
+
+    function pause() external;
+
+    function unpause() external;
 }
